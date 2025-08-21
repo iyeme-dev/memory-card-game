15e894a6beeaf40e15ecc31de1eb8284336b25a2
iyeme-dev <iyemeaji@yahoo.com>
2025-08-21 16:13:29 +0100

    Initialize cards with fetch API

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,12 @@ let timeLeft = 60;
 let timerInterval;
 const timerDisplay = document.querySelector(".timer");
 let timerStarted = false;
+
+fetch("./data/cards.json")
+  .then((res) => res.json())
+  .then((data) => {
+    cards = [...data, ...data];
+    shuffleCards();
+    generateCards();
+  });
+
